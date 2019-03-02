8e09a923784f8e01727459ff766af66993d329c6
Deepak Garg <deepak.garg@cars24.com>
2019-03-02 12:53:06 +0530

    post and retrieval of tasks is working

--- src/main/java/com/example/urbanclap/entities/Task.java
+++ src/main/java/com/example/urbanclap/entities/Task.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.example.urbanclap.enums.Category;
+import com.example.urbanclap.enums.Rating;
+import com.example.urbanclap.enums.TaskStatus;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -25,7 +29,15 @@ public class Task {
 
     private String name;
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private Category category;
+
     private String gPlusLocation;
     private LocalDateTime dateTime;
+    
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    
+    private Rating rating;
 }
